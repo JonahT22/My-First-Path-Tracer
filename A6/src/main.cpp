86c3bedf9ee63259d988b2c5b0f2fc72b1a007c9
JonahT22 <jonahtaylor@att.net>
2021-04-16 18:33:16 -0500

    Create SceneObject and Sphere class to handle intersections

--- A6/src/main.cpp
+++ A6/src/main.cpp
@@ -12,6 +12,12 @@
 using namespace std;
 using namespace glm;
 
+struct Transform {
+	vec3 location = vec3(0, 0, 0);
+	vec3 rotation = vec3(0, 0, 0);
+	vec3 scale = vec3(1, 1, 1);
+};
+
 struct Ray3D {
 	vec3 start;
 	vec3 dir;
@@ -24,32 +30,47 @@ struct HitResult {
 	vec3 color;
 };
 
-bool SphereIntersect(Ray3D ray, HitResult& hit) {
-	// Assume ray direction is normalized, and sphere is at origin with radius 1
-	// a = 1
-	double b = 2.0 * dot(ray.dir, ray.start);
-	double c = dot(ray.start, ray.start) - 1.0;
-	double d2 = pow(b, 2) - (4 * c);
-	if (d2 > 0) {
-		double t1 = (-1.0 * b + sqrt(d2)) / 2.0;
-		double t2 = (-1.0 * b - sqrt(d2)) / 2.0;
-		if (t1 > 0 && t1 < t2) {
-			cout << "Hit at time " << t1 << endl;
-			return true;
+class SceneObject3D {
+public:
+	SceneObject3D() = default;
+
+	virtual bool Intersect(Ray3D ray, HitResult& outHit) = 0;
+
+private:
+	Transform trans;
+};
+
+class Sphere : public SceneObject3D {
+public:
+	Sphere() = default;
+
+	bool Intersect(Ray3D ray, HitResult& outHit) override {
+		// Assume ray direction is normalized, and sphere is at origin with radius 1
+		// a = 1
+		double b = 2.0 * dot(ray.dir, ray.start);
+		double c = dot(ray.start, ray.start) - 1.0;
+		double d2 = pow(b, 2) - (4 * c);
+		if (d2 > 0) {
+			double t1 = (-1.0 * b + sqrt(d2)) / 2.0;
+			double t2 = (-1.0 * b - sqrt(d2)) / 2.0;
+			if (t1 > 0 && t1 < t2) {
+				cout << "Hit at time " << t1 << endl;
+				return true;
+			}
+			else if (t2 > 0 && t2 < t1) {
+				cout << "Hit at time " << t1 << endl;
+				return true;
+			}
+			else return false;
 		}
-		else if (t2 > 0 && t2 < t1) {
-			cout << "Hit at time " << t1 << endl;
+		else if (d2 == 0) {
+			double t = -1.0 * b / 2.0;
+			cout << "Hit at time " << t << endl;
 			return true;
 		}
 		else return false;
 	}
-	else if (d2 == 0) {
-		double t = -1.0 * b / 2.0;
-		cout << "Hit at time " << t << endl;
-		return true;
-	}
-	else return false;
-}
+};
 
 int main(int argc, char **argv)
 {
@@ -68,8 +89,7 @@ int main(int argc, char **argv)
 	double camAspect = 1.0f;
 	
 	// Hard-code a sphere into the middle of the scene
-	vec3 spherePos = vec3(0, 0, 0);
-	double radius = 1.0;
+	shared_ptr<SceneObject3D> testSphere = make_shared<Sphere>();
 
 	// Find distance to image plane so that it goes from -1 to 1 in the y direction
 	double camDist = 1 / tan(camFOVy / 2.0);
@@ -86,7 +106,7 @@ int main(int argc, char **argv)
 			newRay.dir = normalize(newRay.dir);
 			//cout << newRay.dir.x << endl << newRay.dir.y << endl << newRay.dir.z << endl << endl;
 			HitResult hit;
-			if (SphereIntersect(newRay, hit)) {
+			if (testSphere->Intersect(newRay, hit)) {
 				cout << "Hit at point (" << hit.loc.x << ", " << hit.loc.y << ", " << hit.loc.z << ")" << endl;
 				outputImage->setPixel(col, row, 255, 0, 0);
 			}
