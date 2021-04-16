e1c3ee77618c5343ae41a60218715581250a9fd5
JonahT22 <jonahtaylor@att.net>
2021-04-16 17:54:12 -0500

    Add basic sphere intersection

--- A6/src/main.cpp
+++ A6/src/main.cpp
@@ -18,6 +18,39 @@ struct Ray3D {
 	Ray3D(vec3 _start, vec3 _dir) : start(_start), dir(_dir) {}
 };
 
+struct HitResult {
+	vec3 loc;
+	vec3 nor;
+	vec3 color;
+};
+
+bool SphereIntersect(Ray3D ray, HitResult& hit) {
+	// Assume ray direction is normalized, and sphere is at origin with radius 1
+	// a = 1
+	double b = 2.0 * dot(ray.dir, ray.start);
+	double c = dot(ray.start, ray.start) - 1.0;
+	double d2 = pow(b, 2) - (4 * c);
+	if (d2 > 0) {
+		double t1 = (-1.0 * b + sqrt(d2)) / 2.0;
+		double t2 = (-1.0 * b - sqrt(d2)) / 2.0;
+		if (t1 > 0 && t1 < t2) {
+			cout << "Hit at time " << t1 << endl;
+			return true;
+		}
+		else if (t2 > 0 && t2 < t1) {
+			cout << "Hit at time " << t1 << endl;
+			return true;
+		}
+		else return false;
+	}
+	else if (d2 == 0) {
+		double t = -1.0 * b / 2.0;
+		cout << "Hit at time " << t << endl;
+		return true;
+	}
+	else return false;
+}
+
 int main(int argc, char **argv)
 {
 	if(argc < 4) {
@@ -35,6 +68,8 @@ int main(int argc, char **argv)
 	double camAspect = 1.0f;
 	
 	// Hard-code a sphere into the middle of the scene
+	vec3 spherePos = vec3(0, 0, 0);
+	double radius = 1.0;
 
 	// Find distance to image plane so that it goes from -1 to 1 in the y direction
 	double camDist = 1 / tan(camFOVy / 2.0);
@@ -45,10 +80,19 @@ int main(int argc, char **argv)
 		double v = (2.0 * ((double)row + 0.5) / (double)imageSize) - 1.0;
 		for (int col = 0; col < imageSize; col++) {
 			double u = (2.0 * ((double)col + 0.5) / (double)imageSize) - 1.0;
+			//cout << "u: " << u << endl;
+			//cout << "v: " << v << endl;
 			Ray3D newRay = Ray3D(vec3(camPos), vec3(u * camAspect, v, -1.0 * camDist));
 			newRay.dir = normalize(newRay.dir);
+			//cout << newRay.dir.x << endl << newRay.dir.y << endl << newRay.dir.z << endl << endl;
+			HitResult hit;
+			if (SphereIntersect(newRay, hit)) {
+				cout << "Hit at point (" << hit.loc.x << ", " << hit.loc.y << ", " << hit.loc.z << ")" << endl;
+				outputImage->setPixel(col, row, 255, 0, 0);
+			}
 		}
 	}
+	outputImage->writeToFile(fileName);
 	
 	return 0;
 }
