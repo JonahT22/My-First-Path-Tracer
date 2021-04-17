db9577818af89b4842ca4bbfdec45293ee1b4b3d
JonahT22 <jonahtaylor@att.net>
2021-04-16 19:08:00 -0500

    Move camera properties to struct

--- A6/src/main.cpp
+++ A6/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 #define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
@@ -8,6 +9,7 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 #include "Image.h"
+#include "MatrixStack.h"
 
 using namespace std;
 using namespace glm;
@@ -25,9 +27,19 @@ struct Ray3D {
 };
 
 struct HitResult {
+	// Initilialize as largest possible double value
+	double tMin = numeric_limits<double>::max();
 	vec3 loc;
 	vec3 nor;
 	vec3 color;
+};
+
+struct Camera {
+	vec3 pos = vec3(0, 0, 0);
+	double fovY = 90;
+	double aspect = 1.0;
+	Camera(vec3 _pos, double _fov, int width, int height)
+		: pos(_pos), fovY(_fov), aspect(width / (double)height) {}
 };
 
 class SceneObject3D {
@@ -79,35 +91,36 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	int sceneNum = atoi(argv[1]);
-	int imageSize = atoi(argv[2]);
+	int height = atoi(argv[2]);
+	int width = height;
 	string fileName(argv[3]);
-	shared_ptr<Image> outputImage = make_shared<Image>(imageSize, imageSize);
+	shared_ptr<Image> outputImage = make_shared<Image>(width, height);
 
-	vec3 camPos = vec3(0, 0, 5);
-	constexpr double camFOVy = 45.0 * pi<double>() / 180.0;
-	// aspect ratio, which gets multiplied by the X value (since our fov is in respect to the y direction)
-	double camAspect = 1.0f;
+	Camera camera = Camera(
+		vec3(0, 0, 5),
+		45.0 * pi<double>() / 180.0, 
+		width, height);
 	
 	// Hard-code a sphere into the middle of the scene
 	shared_ptr<SceneObject3D> testSphere = make_shared<Sphere>();
 
 	// Find distance to image plane so that it goes from -1 to 1 in the y direction
-	double camDist = 1 / tan(camFOVy / 2.0);
+	double camDist = 1 / tan(camera.fovY / 2.0);
 	//imageSize -= 1;
 	// Generate rays from the camera to the center of each pixel
-	for (int row = 0; row < imageSize; row++) {
+	for (int row = 0; row < height; row++) {
 		// u and v are in normalized image coords, -1 to 1
-		double v = (2.0 * ((double)row + 0.5) / (double)imageSize) - 1.0;
-		for (int col = 0; col < imageSize; col++) {
-			double u = (2.0 * ((double)col + 0.5) / (double)imageSize) - 1.0;
+		double v = (2.0 * ((double)row + 0.5) / (double)height) - 1.0;
+		for (int col = 0; col < width; col++) {
+			double u = (2.0 * ((double)col + 0.5) / (double)width) - 1.0;
 			//cout << "u: " << u << endl;
 			//cout << "v: " << v << endl;
-			Ray3D newRay = Ray3D(vec3(camPos), vec3(u * camAspect, v, -1.0 * camDist));
+			Ray3D newRay = Ray3D(vec3(camera.pos), vec3(u * camera.aspect, v, -1.0 * camDist));
 			newRay.dir = normalize(newRay.dir);
 			//cout << newRay.dir.x << endl << newRay.dir.y << endl << newRay.dir.z << endl << endl;
 			HitResult hit;
 			if (testSphere->Intersect(newRay, hit)) {
-				cout << "Hit at point (" << hit.loc.x << ", " << hit.loc.y << ", " << hit.loc.z << ")" << endl;
+				//cout << "Hit at point (" << hit.loc.x << ", " << hit.loc.y << ", " << hit.loc.z << ")" << endl;
 				outputImage->setPixel(col, row, 255, 0, 0);
 			}
 		}
