a799f544fc5ab6f13ef3d8979e039b089a4052ed
JonahT22 <jonahtaylor@att.net>
2021-04-24 14:59:49 -0500

    Simple lambertian shading

--- A6/src/Scene.cpp
+++ A6/src/Scene.cpp
@@ -13,6 +13,9 @@ glm::vec3 Scene::ComputeRayColor(Ray3D ray)
 		if (object->Hit(ray, hit)) {
 			// If hit was successfull (i.e. found a new tMin), store a reference to the object
 			hit.hitObject = object;
+			// TODO problem: do I need a tMax in the Hit function if this only returns true when it finds something smaller
+			// than the default value of t in the hit? I.e. instead of setting tMax at a number, just set the default value of t
+			// to that number
 		}
 	}
 
@@ -27,12 +30,19 @@ glm::vec3 Scene::ComputeRayColor(Ray3D ray)
 		// During intersection checks, hit.nor is filled with local-space normal. Now, convert to world space
 		hit.nor = normalize(hit.hitObject->GetInverseTranspose() * hit.nor);
 
-		// Color with normals
-		// Normal -> color conversion taken from assigment handout
-		double r = (0.5 * hit.nor.x + 0.5);
-		double g = (0.5 * hit.nor.y + 0.5);
-		double b = (0.5 * hit.nor.z + 0.5);
-		return vec3(r, g, b);
+		// Calculate lighting using simple lambertian shading model
+		vec4 lightPos(1, 1, 0, 1);
+		double intensity = std::max(0.0f, dot(hit.nor, normalize(lightPos - hit.loc)));
+
+		// Color Red w/lambertian shading
+		return vec3(intensity, 0, 0);
+
+		//// Color with normals
+		//// Normal -> color conversion taken from assigment handout
+		//double r = 0.5 * hit.nor.x + 0.5;
+		//double g = 0.5 * hit.nor.y + 0.5;
+		//double b = 0.5 * hit.nor.z + 0.5;
+		//return vec3(r, g, b);
 	}
 	// Background color
 	else return glm::vec3(0, 0, 0);
@@ -43,7 +53,7 @@ void Scene::BuildSceneFromFile(std::string filename)
 	// Hard-code a sphere into the middle of the scene
 	shared_ptr<SceneObject> testSphere = make_shared<Sphere>();
 	testSphere->CreateTransformMtx(Transform(
-		vec4(0, 0, 0, 1),
+		vec4(-1, 0, 0, 1),
 		vec3(0, 0, .75),
 		vec3(.5, 1, 1)));
 	allObjects.push_back(testSphere);
