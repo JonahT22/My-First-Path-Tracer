7f7fffcad2d0414c4305f222029a9d3c36ed975d
JonahT22 <jonahtaylor@att.net>
2021-04-27 16:16:57 -0500

    Add more descriptive comments to main

--- A6/src/main.cpp
+++ A6/src/main.cpp
@@ -43,23 +43,27 @@ int main(int argc, char **argv) {
 		cout << "Usage: ./A6 <SCENENUMBER> <IMAGE SIZE> <IMAGE FILENAME>" << endl;
 		return 0;
 	}
-	// TODO: maybe remove this?
 	int sceneNum = atoi(argv[1]);
 	int height = atoi(argv[2]);
 	int width = height;
 	string fileName(argv[3]);
 	shared_ptr<Image> outputImage = make_shared<Image>(width, height);
 
+	// Provide image dimensions to camera for aspect ratio & ray calculations
 	Camera camera (width, height);
 
+	// Build a scene with a black background color
 	Scene scene(dvec3(0, 0, 0));
 	scene.BuildSceneFromFile("../resources/scene" + string(argv[1]) + ".txt", camera);
 	
-	// Generate rays from the camera to the center of each pixel
+	// Iterate over every pixel
 	for (int row = 0; row < height; row++) {
 		for (int col = 0; col < width; col++) {
+			// Generate rays from the camera to the center of the current pixel
 			Ray3D newRay = camera.CreateCameraRay(row, col);
+			// Iterate over every item in the scene to find the intersection/color of the ray
 			dvec3 rayColor = scene.ComputeRayColor(newRay);
+			// Store color value
 			outputImage->setPixel(col, row, 255 * rayColor.r, 255 * rayColor.g, 255 * rayColor.b);
 		}
 	}
