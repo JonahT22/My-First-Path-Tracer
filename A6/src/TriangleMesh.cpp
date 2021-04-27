4ca9b60a0b672e85f11e8a3cf222dbc2371538b2
JonahT22 <jonahtaylor@att.net>
2021-04-26 23:07:43 -0500

    Import obj-loading code from A1

--- A6/src/TriangleMesh.cpp
+++ A6/src/TriangleMesh.cpp
@@ -42,4 +42,72 @@ void TriangleMesh::Init() {
 }
 
 void TriangleMesh::PopulateTriangleArray(std::string filename) {
+//LOAD GEOMETRY
+//v Starter Code begins here v
+	vector<float> posBuf; // list of vertex positions
+	vector<float> norBuf; // list of vertex normals
+	vector<float> texBuf; // list of vertex texture coords
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	string errStr;
+	bool rc = tinyobj::LoadObj(&attrib, &shapes, &materials, &errStr, filename.c_str());
+	if (!rc) {
+		cerr << errStr << endl;
+	}
+	else {
+		// Some OBJ files have different indices for vertex positions, normals,
+		// and texture coordinates. For example, a cube corner vertex may have
+		// three different normals. Here, we are going to duplicate all such
+		// vertices.
+		// Loop over shapes
+		for (size_t s = 0; s < shapes.size(); s++) {
+			// Loop over faces (polygons)
+			size_t index_offset = 0;
+			for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+				size_t fv = shapes[s].mesh.num_face_vertices[f];
+				// Loop over vertices in the face.
+				for (size_t v = 0; v < fv; v++) {
+					// access to vertex
+					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+					posBuf.push_back(attrib.vertices[3 * idx.vertex_index + 0]);
+					posBuf.push_back(attrib.vertices[3 * idx.vertex_index + 1]);
+					posBuf.push_back(attrib.vertices[3 * idx.vertex_index + 2]);
+					if (!attrib.normals.empty()) {
+						norBuf.push_back(attrib.normals[3 * idx.normal_index + 0]);
+						norBuf.push_back(attrib.normals[3 * idx.normal_index + 1]);
+						norBuf.push_back(attrib.normals[3 * idx.normal_index + 2]);
+					}
+					if (!attrib.texcoords.empty()) {
+						texBuf.push_back(attrib.texcoords[2 * idx.texcoord_index + 0]);
+						texBuf.push_back(attrib.texcoords[2 * idx.texcoord_index + 1]);
+					}
+				}
+				index_offset += fv;
+				// per-face material (IGNORE)
+				shapes[s].mesh.material_ids[f];
+			}
+		}
+	}
+	cout << "Number of vertices: " << posBuf.size() / 3 << endl;
+	//^ Starter code ends here ^
+
+	// Read through the pos buf, creating the triangles
+	for (int i = 0; i < posBuf.size(); i += 9) {
+		shared_ptr<Triangle> newTri = make_shared<Triangle>(); //create a new triangle
+
+		//Set the new triangle's vertex positions
+		newTri->SetLocations(
+			dvec4(posBuf.at(i + 0), posBuf.at(i + 1), posBuf.at(i + 2), 1),
+			dvec4(posBuf.at(i + 3), posBuf.at(i + 4), posBuf.at(i + 5), 1),
+			dvec4(posBuf.at(i + 6), posBuf.at(i + 7), posBuf.at(i + 8), 1)
+		);
+		newTri->SetNorms(
+			dvec4(norBuf.at(i + 0), norBuf.at(i + 1), norBuf.at(i + 2), 0),
+			dvec4(norBuf.at(i + 3), norBuf.at(i + 4), norBuf.at(i + 5), 0),
+			dvec4(norBuf.at(i + 6), norBuf.at(i + 7), norBuf.at(i + 8), 0)
+		);
+		allTriangles.push_back(newTri);
+	}
+	cout << "Number of triangles: " << allTriangles.size() << endl;
 }
