4b6959ff806214d6a18e6f2360d5f3522734cbf5
JonahT22 <jonahtaylor@att.net>
2021-05-14 18:37:46 -0500

    Update pointlights to inherit from new light class

--- src/PointLight.h
+++ src/PointLight.h
@@ -1,19 +1,30 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <memory>
 #include <string>
+#include "Light.h"
 
-struct PointLight {
-	std::string name;
-	glm::dvec4 pos;
-	double intensity;
+// An infinitely small light with position and color
+struct PointLight : public Light {
+	// Call parent constructor
+	PointLight(std::string _name, double _L, double _Q, double _falloffDistance, glm::dvec4 _loc, glm::dvec3 _color) :
+		Light(_name, _L, _Q, _falloffDistance), 
+		loc(_loc),
+		color(_color) {}
 
-	PointLight() :
-		name("unnamed_light"),
-		pos(glm::dvec4(0, 0, 0, 1)),
-		intensity(1.0) {}
-	PointLight(std::string _name, glm::dvec4 _pos, double _intensity) :
-		name(_name),
-		pos(_pos),
-		intensity(_intensity) {}
+	glm::dvec3 GetColor() override {
+		// TODO: make sceneobject materials into a shared ptr
+		return color;
+	}
+	glm::dvec4 GetLocation() override {
+		return loc;
+	}
+	// Returns null, since point lights are not attached to a particular object
+	std::shared_ptr<SceneObject> GetObject() override {
+		return nullptr;
+	}
+private:
+	glm::dvec4 loc = glm::dvec4(0, 0, 0, 1);
+	glm::dvec3 color = glm::dvec3(1, 1, 1);
 };
