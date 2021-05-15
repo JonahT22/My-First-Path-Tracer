d5581cdd20851a13fdf7f3934d0794cc79461c3b
JonahT22 <jonahtaylor@att.net>
2021-05-15 14:37:09 -0500

    calculate box hit normals

--- src/Box.cpp
+++ src/Box.cpp
@@ -8,35 +8,51 @@ bool Box::IntersectLocal(Ray3D& ray, HitResult& outHit, double tMin, double tMax
 	// Optimized ray-box intersection adapted from http://people.csail.mit.edu/amy/papers/box-jgt.pdf
 
 	// Assume box is at the origin, axis-aligned, with bounds from -0.5 to 0.5 in x, y, and z
-	// Bounds are the same in the x, y, and z directions
-	double bounds[2] = {-0.5, 0.5};
+	// tvals[0] corresponds to the closer hit, tvals[1] is the farther one
 	double tvals[2];
+	// The normals of each the side of the box that each tval corresponds to
+	dvec4 normals[2];
 
-	// Start with the t values from x-axis intersections
-	// Swap the negative/positive bounds when the ray direction is negative (since far/near sides will be opposite)
-	tvals[0] = (bounds[ray.sign[0]] - ray.start.x) * ray.invDir.x;
-	tvals[1] = (bounds[1 - ray.sign[0]] - ray.start.x) * ray.invDir.x;
+	// Start with the t values & normals from x-axis intersections
+	// Flip sign of the negative/positive bounds when the ray direction is negative (since far/near sides will be swapped)
+	tvals[0] = ((-0.5 * ray.sign[0]) - ray.start.x) * ray.invDir.x;
+	tvals[1] = (( 0.5 * ray.sign[0]) - ray.start.x) * ray.invDir.x;
+	normals[0] = (double)ray.sign[0] * dvec4( 1, 0, 0, 0);
+	normals[1] = (double)ray.sign[0] * dvec4(-1, 0, 0, 0);
 
 	// Find min/max t in y direction
-	double tymin = (bounds[ray.sign[1]] - ray.start.y) * ray.invDir.y;
-	double tymax = (bounds[1 - ray.sign[1]] - ray.start.y) * ray.invDir.y;
+	double tymin = ((-0.5 * ray.sign[1]) - ray.start.y) * ray.invDir.y;
+	double tymax = (( 0.5 * ray.sign[1]) - ray.start.y) * ray.invDir.y;
 
 	// If new y min/max are completely outside the current t min/max, return false immediately
 	if ((tvals[0] > tymax) || (tymin > tvals[1])) {
 		return false;
 	}
 	// Check if new min/max values are closer to the center of the box
-	if (tymin > tvals[0]) tvals[0] = tymin;
-	if (tymax < tvals[1]) tvals[1] = tymax;
+	if (tymin > tvals[0]) {
+		tvals[0] = tymin;
+		// If a new tval is found, update the corresponding normal as well
+		normals[0] = (double)ray.sign[1] * dvec4(0, -1, 0, 0);
+	}
+	if (tymax < tvals[1]) {
+		tvals[1] = tymax;
+		normals[1] = (double)ray.sign[1] * dvec4(0, 1, 0, 0);
+	}
 
 	// Repeat the same process as above, but with the z direction
-	double tzmin = (bounds[ray.sign[2]] - ray.start.z) * ray.invDir.z;
-	double tzmax = (bounds[1 - ray.sign[2]] - ray.start.z) * ray.invDir.z;
+	double tzmin = ((-0.5 * ray.sign[2]) - ray.start.z) * ray.invDir.z;
+	double tzmax = (( 0.5 * ray.sign[2]) - ray.start.z) * ray.invDir.z;
 	if ((tvals[0] > tzmax) || (tzmin > tvals[1])) {
 		return false;
 	}
-	if (tzmin > tvals[0]) tvals[0] = tzmin;
-	if (tzmax < tvals[1]) tvals[1] = tzmax;
+	if (tzmin > tvals[0]) {
+		tvals[0] = tzmin;
+		normals[0] = (double)ray.sign[2] * dvec4(0, 0, -1, 0);
+	}
+	if (tzmax < tvals[1]) {
+		tvals[1] = tzmax;
+		normals[1] = (double)ray.sign[2] * dvec4(0, 0, 1, 0);
+	}
 
 	// Choose the smallest t value within the valid range, and choose the corresponding normal
 	int chosenIdx = SelectSmallestInRange(tvals, tMin, tMax);
@@ -44,7 +60,7 @@ bool Box::IntersectLocal(Ray3D& ray, HitResult& outHit, double tMin, double tMax
 		// Try updating the HitResult with whichever t value was the smallest in the range
 		if (outHit.UpdateTMin(tvals[chosenIdx])) {
 			// If the HitResult ended up finding a new minT, update the normal value in the hit result and return true
-			outHit.nor = dvec4(0, 1, 0, 0);
+			outHit.nor = normals[chosenIdx];
 			return true;
 		}
 	}
--- src/Ray3D.h
+++ src/Ray3D.h
@@ -4,15 +4,15 @@
 
 struct Ray3D {
 	Ray3D(glm::dvec4 _start, glm::dvec4 _dir) : start(_start), dir(_dir), invDir(1.0 / _dir) {
-		sign[0] = (invDir.x < 0);
-		sign[1] = (invDir.y < 0);
-		sign[2] = (invDir.z < 0);
+		sign[0] = (invDir.x < 0) ? -1 : 1;
+		sign[1] = (invDir.y < 0) ? -1 : 1;
+		sign[2] = (invDir.z < 0) ? -1 : 1;
 	}
 	glm::dvec4 start;
 	glm::dvec4 dir;
 	// Cache reciprocal of direction for ray-box intersections
 	glm::dvec4 invDir;
-	// Signs of the x, y, and z components of invDir (used in ray-box intersections)
+	// Signs of the x, y, and z components of invDir, either -1 or 1 (used in ray-box intersections)
 	int sign[3];
 
 	glm::dvec4 FindLocAtTime(double time) {
